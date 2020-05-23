b9ba1959fcc5a0e1958941a668c434aa1aeecae6
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-05-22 19:34:52 -0700

    Finished auth router, now able to logout

--- model/model.js
+++ model/model.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 // const jwt = require('jsonwebtoken')
-const db = require('../dbconfig')
+const db = require('../database/dbconfig')
 
 async function findBy(filter) {
 	return db("users")
